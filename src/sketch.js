deee6fafaa411b985ca0262522011aa4275f10cc
ikws4 <zhipingne@gmail.com>
2021-12-11 10:54:00 +0800

    feat: add more rose-pine palette

--- src/sketch.js
+++ src/sketch.js
@@ -1,21 +1,37 @@
 /// <reference path="../node_modules/@types/p5/global.d.ts" />
 
 // rose-pine colorscheme {{{
-let p_base = "#232136";
-let p_surface = "#2a273f";
-let p_overlay = "#393552";
-let p_inactive = "#59546d";
-let p_subtle = "#817c9c";
+let p_base = "#191724";
+let p_surface = "#1f1d2e";
+let p_overlay = "#26233a";
+let p_inactive = "#555169";
+let p_subtle = "#6e6a86";
 let p_text = "#e0def4";
 let p_love = "#eb6f92";
 let p_gold = "#f6c177";
-let p_rose = "#ea9a97";
-let p_pine = "#3e8fb0";
+let p_rose = "#ebbcba";
+let p_pine = "#31748f";
 let p_foam = "#9ccfd8";
 let p_iris = "#c4a7e7";
-let p_highlight = "#312f44";
-let p_highlight_inactive = "#2a283d";
-let p_highlight_overlay = "#3f3c53";
+let p_highlight = "#2a2837";
+let p_highlight_inactive = "#211f2d";
+let p_highlight_overlay = "#3a384a";
+
+// let p_base = "#232136";
+// let p_surface = "#2a273f";
+// let p_overlay = "#393552";
+// let p_inactive = "#59546d";
+// let p_subtle = "#817c9c";
+// let p_text = "#e0def4";
+// let p_love = "#eb6f92";
+// let p_gold = "#f6c177";
+// let p_rose = "#ea9a97";
+// let p_pine = "#3e8fb0";
+// let p_foam = "#9ccfd8";
+// let p_iris = "#c4a7e7";
+// let p_highlight = "#312f44";
+// let p_highlight_inactive = "#2a283d";
+// let p_highlight_overlay = "#3f3c53";
 
 // let p_base = "#faf4ed";
 // let p_surface = "#fffaf3";
@@ -35,11 +51,10 @@ let p_highlight_overlay = "#3f3c53";
 // }}}
 
 function setup() {
-  createCanvas(400, 400)
+  createCanvas(400, 400);
 }
 
 function draw() {
-  stroke(p_text)
-  ellipse(10, 10, 20, 20)
+  stroke(p_text);
+  ellipse(10, 10, 20, 20);
 }
-
