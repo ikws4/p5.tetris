3499bedbc7bfe600f1e3385748ddddfeb8afe122
ikws4 <zhipingne@gmail.com>
2021-12-11 09:59:43 +0800

    refactor: using ts

--- src/sketch.js
+++ src/sketch.js
@@ -1,3 +1,5 @@
+/// <reference path="../node_modules/@types/p5/global.d.ts" />
+
 // rose-pine colorscheme {{{
 let p_base = "#232136";
 let p_surface = "#2a273f";
@@ -32,131 +34,12 @@ let p_highlight_overlay = "#3f3c53";
 // let p_highlight_overlay = "#e4dfde";
 // }}}
 
-// Gui setup {{{
-let gui;
-let sidebarWidth;
-let sidebarHeight;
-let sketchWidth;
-let sketchHeight;
-let isStart = false;
-
-function createGUI() {
-  gui = createGui();
-  gui.loadStyle("Seafoam");
-
-  sidebarWidth = width * 0.4;
-  sidebarHeight = height;
-  sketchWidth = width - sidebarWidth;
-  sketchHeight = height;
-
-  // define control button
-  const buttonStyle = {
-    textSize: 12,
-    rounding: 6,
-  };
-  const buttons = [
-    {
-      lable: "PLAY",
-      onPressed: onPlayButtonPressed,
-    },
-    {
-      lable: "STOP",
-      onPressed: onStopButtonPressed,
-    },
-    {
-      lable: "RESET",
-      onPressed: onResetButtonPressed,
-    },
-  ];
-
-  const buttonGap = 16;
-  const buttonWidth =
-    (sidebarWidth - buttonGap * (buttons.length + 1)) / buttons.length;
-
-  buttons.forEach((item, index) => {
-    let x = index * buttonWidth + buttonGap * (index + 1);
-    let button = createButton(item.lable, x, 16, buttonWidth, 32);
-    button.setStyle(buttonStyle);
-    button.onPress = item.onPressed;
-  });
-
-  sidebarGui(0, 64);
-}
-
-function drawGUI() {
-  drawGui();
-
-  stroke(p_overlay);
-  strokeWeight(2);
-  line(sidebarWidth, 0, sidebarWidth, height);
-  line(0, 64, sidebarWidth, 64);
-}
-// }}}
-
-// Callback functions {{{
-function onPlayButtonPressed() {
-  isStart = true;
-}
-
-function onStopButtonPressed() {
-  isStart = false;
-}
-
-function onResetButtonPressed() {
-  background(p_base);
-  resetSketch();
-}
-// }}}
-
-// Internal {{{
 function setup() {
-  createCanvas(600, 400);
-  createGUI();
-  background(p_base);
+  createCanvas(400, 400)
 }
 
 function draw() {
-  if (isStart) {
-    background(p_base);
-  }
-
-  push();
-  {
-    translate(sidebarWidth, 0);
-    if (isStart) {
-      drawSketch();
-    } else {
-      drawPausedOverlay();
-    }
-  }
-  pop();
-
-  noStroke();
-  fill(p_surface);
-  rect(0, 0, sidebarWidth, sidebarHeight);
-  drawGUI();
+  stroke(p_text)
+  ellipse(10, 10, 20, 20)
 }
 
-function drawPausedOverlay() {
-  push();
-  {
-    translate(sketchWidth / 2, sketchHeight / 2);
-    rotate(PI / 2);
-    noStroke();
-    fill(p_overlay);
-    ellipse(0, 0, 64, 64);
-    fill(p_subtle);
-    triangle(-15, 10, 0, -20, 15, 10);
-  }
-  pop();
-}
-// }}}
-
-function resetSketch() {
-}
-
-function sidebarGui(originX, originY) {
-}
-
-function drawSketch() {
-}
